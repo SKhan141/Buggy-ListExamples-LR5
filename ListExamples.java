9ea4b5347984066afc0d7875a58b9c174aa5efae
SKhan141 <105250752+SKhan141@users.noreply.github.com>
2023-12-04 14:22:50 -0800

    Buggy ListExamples

--- ListExamples.java
+++ ListExamples.java
@@ -24,23 +24,19 @@ class ListExamples {
   static List<String> merge(List<String> list1, List<String> list2) {
     List<String> result = new ArrayList<>();
     int index1 = 0, index2 = 0;
+    // Introducing a bug by not checking if the lists are sorted
     while(index1 < list1.size() && index2 < list2.size()) {
-      if(list1.get(index1).compareTo(list2.get(index2)) < 0) {
-        result.add(list1.get(index1));
-        index1 += 1;
-      }
-      else {
+        // Simulating a wrong comparison by always adding from list2
         result.add(list2.get(index2));
         index2 += 1;
-      }
     }
     while(index1 < list1.size()) {
-      result.add(list1.get(index1));
-      index1 += 1;
+        result.add(list1.get(index1));
+        index1 += 1;
     }
     while(index2 < list2.size()) {
-      result.add(list2.get(index2));
-      index2 += 1;
+        result.add(list2.get(index2));
+        index2 += 1;
     }
     return result;
   }
