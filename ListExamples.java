8a4d574a2bd1f2315f2a1839e973ea29da341732
SKhan141 <105250752+SKhan141@users.noreply.github.com>
2023-12-04 16:05:49 -0800

    "Fixing" bugs

--- ListExamples.java
+++ ListExamples.java
@@ -26,9 +26,14 @@ class ListExamples {
     int index1 = 0, index2 = 0;
     // Introducing a bug by not checking if the lists are sorted
     while(index1 < list1.size() && index2 < list2.size()) {
-        // Simulating a wrong comparison by always adding from list2
-        result.add(list2.get(index2));
-        index2 += 1;
+        if(list1.get(index1).compareTo(list2.get(index2)) < 0) {
+          result.add(list1.get(index1));
+          index1 += 1;
+        } 
+        else {
+          result.add(list2.get(index2));
+          index2 += 1;
+        }
     }
     while(index1 < list1.size()) {
         result.add(list1.get(index1));
