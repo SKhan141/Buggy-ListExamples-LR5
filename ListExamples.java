22ded43587d4539f2f18542d82753e48bb79574e
SKhan141 <105250752+SKhan141@users.noreply.github.com>
2023-12-04 22:14:57 -0800

    back to buggy

--- ListExamples.java
+++ ListExamples.java
@@ -26,14 +26,8 @@ class ListExamples {
     int index1 = 0, index2 = 0;
     // Introducing a bug by not checking if the lists are sorted
     while(index1 < list1.size() && index2 < list2.size()) {
-        if(list1.get(index1).compareTo(list2.get(index2)) < 0) {
-          result.add(list1.get(index1));
-          index1 += 1;
-        } 
-        else {
           result.add(list2.get(index2));
           index2 += 1;
-        }
     }
     while(index1 < list1.size()) {
         result.add(list1.get(index1));
